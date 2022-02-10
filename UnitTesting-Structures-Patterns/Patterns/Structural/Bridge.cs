a5cf5192a47fd07a05c6bf04681e54fd42e56de0
Andrei Blohin <a.blohin@aol.com>
2022-02-10 17:17:49 +0300

    Added Bridge pattern

---
+++ UnitTesting-Structures-Patterns/Patterns/Structural/Bridge.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace UnitTesting_Structures_Patterns.Patterns.Structural
+{
+    public interface IDrawer
+    {
+        void DrawFigure();
+    }
+
+    public class FiguresShapes
+    {
+        public class Circle : IDrawer
+        {
+            public void DrawFigure()
+            {
+                Console.WriteLine("drawing circle");
+            }
+        }
+
+        public class Triangle : IDrawer
+        {
+            public void DrawFigure()
+            {
+                Console.WriteLine("drawing triangle");
+            }
+        }
+    }
+
+    public abstract class FiguresFilling
+    {
+        protected IDrawer drawer;
+
+        public FiguresFilling(IDrawer drawer)
+        {
+            this.drawer = drawer ?? throw new ArgumentNullException(nameof(drawer));
+        }
+
+        public abstract void Draw();
+
+    }
+
+    public class CharFilling : FiguresFilling
+    {
+        public CharFilling(IDrawer drawer) : base(drawer)
+        {
+        }
+
+        public override void Draw()
+        {
+            drawer.DrawFigure(); Console.WriteLine(" using CHARS");
+        }
+    }
+
+    public class NumFilling : FiguresFilling
+    {
+        public NumFilling(IDrawer drawer) : base(drawer)
+        {
+
+        }
+
+        public override void Draw()
+        {
+            drawer.DrawFigure(); Console.WriteLine(" using NUMS");
+        }
+    }
+
+
+    //var drawer = new Triangle();
+    //var filling = new CharFilling(drawer);
+    //var filling1 = new NumFilling(drawer);
+    //filling.Draw();
+    //        filling1.Draw();
+}
